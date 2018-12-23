38973084dfa1e79dc7db20cdd2f1fe597bfc6614
kevinhuang78 <kevin_huang_iim@yahoo.com>
2018-12-24 00:18:58 +0100

    feat: add live datas and style: some buttons and bg

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('.matchsItem__header').click(function(){
-       $(this).next('.matchsItem__content').toggle();
+       $(this).toggleClass('matchsItem__header--opened');
     });
 });
--- src/Controller/LeagueController.php
+++ src/Controller/LeagueController.php
@@ -68,4 +68,35 @@ class LeagueController extends AbstractController
             'matches' => $result
         ]);
     }
+
+    /**
+     * @Route("/live", name="live")
+     */
+    public function liveMatchesAction()
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $from = date('Y-m-d');
+        $to = date('Y-m-d');
+        $APIkey=$this->getParameter('API_KEY');
+
+        // League ID 127 = Ligue 1 / Country ID 173 = France
+        $curl_options = array(
+            CURLOPT_URL => "https://apifootball.com/api/?action=get_events&from=$from&to=$to&country_id=173&league_id=127&match_live=1&APIkey=$APIkey",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => false,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_CONNECTTIMEOUT => 5
+        );
+
+        $curl = curl_init();
+        curl_setopt_array( $curl, $curl_options );
+        $result = curl_exec( $curl );
+
+        $result = (array) json_decode($result);
+
+        return $this->render('league/live.html.twig', [
+            'matches' => $result
+        ]);
+    }
 }
