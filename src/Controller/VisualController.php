ee95642b3ae54b68b527d52fd83e83ac1f4cd19f
kevinhuang78 <kevin_huang_iim@yahoo.com>
2019-01-15 17:24:16 +0100

    feat: upload photo

--- src/Controller/VisualController.php
+++ src/Controller/VisualController.php
@@ -4,22 +4,29 @@ namespace App\Controller;
 
 use App\Form\VisualType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class VisualController extends AbstractController
 {
     /**
      * @param Request $request
+     * @param UserInterface $user
      * @Route("/visual", name="visualView")
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function visual(Request $request)
+    public function visual(Request $request, UserInterface $user)
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
         $form = $this->createForm(VisualType::class, []);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dd('it works');
+            /** @var UploadedFile $file */
+            $file = $request->files->get('visual')['photo'];
+            $file->move(__DIR__ . '/../../public/uploads/' . $user->getId() . '/', $file->getClientOriginalName());
             return $this->redirectToRoute('visualView');
         }
 
